f0ea889092aa9c09287f8acf8cf6e3c4253f5a1a
ConnorLear <connor.a.lear@gmail.com>
2024-10-04 22:01:02 +0000

    update kattus.py

--- kattus.py
+++ kattus.py
@@ -3,6 +3,33 @@ Kattus problems most go here.
 We define each thing so we can save things
 and so we can write multply things
 """
-
 def p1():
-    print("")
+
+    problem = str(input())
+    a, b, c = problem.split()
+    a = int(a)
+    b = int(b)
+    c = int(c)
+
+    if a+b == c:
+        print("correct!")
+    else:
+        print("wrong!")
+def p2():
+    problem2 = str(input())
+    a, b = problem2.split()
+    a = int(a)
+    b = int(b)
+    print(a+b)
+
+
+
+
+
+
+
+
+
+
+
+print("")
