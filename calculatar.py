6cbff9fc09ff43f6afadba269dacfbb3c158c1f6
ConnorLear <connor.a.lear@gmail.com>
2024-10-09 19:09:02 +0000

    update

--- calculatar.py
+++ calculatar.py
@@ -8,9 +8,9 @@ A. Adding
 B. Subtracting
 C. Multiplication
 D. Divition
-E. Geometry
-F. Algebra
-G. Variable
+E. Variable
+D. Exponent 
+F. Multiple Problems
 """)
 
 """
@@ -21,6 +21,9 @@ n = problem letter
 d = decimal
 """
 
+
+import math
+
 #Persons answer
 pm = input().lower()
 p("")
@@ -28,25 +31,74 @@ p("")
 #Addition
 if pm == "a":
     lista =[]
-    n1 = (input("How many numbers are their to add and is there a decimal(y/n): "))
-    x1, y1 = n1.split()
+    x1 = int(input("How many numbers are their to Add: "))
+    y1 = str(input("Is one of your numbers a decimal(y/n): "))
     p("")
     p("What numbers:")
     if y1 == "y":
-        
         for i in range(x1):
-            y2 = float(input())
-            
+            y2 = float(input(">"))
+            lista.append(y2)
+        print("Answer: ",sum(lista))
     else:
-        print("")        
+        for i in range(x1):
+            y2 = int(input(">"))
+            lista.append(y2)
+        print("Answer: ",sum(lista))
 
-
+#Subtraction
 elif pm == "b":
+    lista =[]
+    x1 = int(input("How many numbers are their to Subtract: "))
+    y1 = str(input("Is one of your numbers a decimal(y/n):" ))
     p("")
+    p("What numbers:")
+    if y1 == "y":
+        for i in range(x1):
+            y2 = float(input(">"))
+            lista.append(y2)
+        print("Answer: ",zip(lista))
+    else:
+        for i in range(x1):
+            y2 = int(input(">"))
+            lista.append(y2)
+        print("Answer: ",zip(lista))
+
+#Multpication 
 elif pm == "c":
+    lista =[]
+    x1 = int(input("How many numbers are their to Multiply: "))
+    y1 = str(input("Is one of your numbers a decimal(y/n):" ))
     p("")
+    p("What numbers:")
+    if y1 == "y":
+        for i in range(x1):
+            y2 = float(input(">"))
+            lista.append(y2)
+        print("Answer: ",math.prod(lista))
+    else:
+        for i in range(x1):
+            y2 = int(input(">"))
+            lista.append(y2)
+        print("Answer: ",math.prod(lista))
+
+#Divition
 elif pm == "d":
+    lista =[]
+    x1 = int(input("How many numbers are their to Divide: "))
     p("")
+    p("What numbers:")
+    for i in range(x1):
+        y2 = float(input(">"))
+            lista.append(y2)
+        print("Answer: ",divmod(lista))
+    else:
+        for i in range(x1):
+            y2 = int(input(">"))
+            lista.append(y2)
+        print("Answer: ",divmod(lista))
+
+#Variables    
 elif pm == "e":
     p("")
 elif pm == "f":
@@ -56,3 +108,6 @@ elif pm == "g":
 else:
     p("""That's not an option.
 Try again""")
+
+
+p("")
