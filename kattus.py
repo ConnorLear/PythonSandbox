d925408e6b63d527265e5d44ae40c62b08e586a4
ConnorLear <connor.a.lear@gmail.com>
2024-10-22 21:15:16 +0000

    Update kattus

--- kattus.py
+++ kattus.py
@@ -1,5 +1,11 @@
+list = []
 n = int(input())
 for i in range(n):
-    list = input()
-p = list[0:1].upper()
-print(p)
+    list1 = input()
+    p1 = list1[0:1].upper()
+    p2 = list1[1:].lower()
+    l1 = (p1+p2)
+    list.append(l1)
+    if i == n-1:
+        for j in range(len(list)):
+            print(list[j])
