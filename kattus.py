da2a782bc643cede09e554bca99482a5e63234a7
ConnorLear <connor.a.lear@gmail.com>
2024-10-24 21:06:03 +0000

    Update kattus.py

--- kattus.py
+++ kattus.py
@@ -1,4 +1,7 @@
-n = input()
-x, z = n.split(" ")
-y = int(z)*2-int(x)
-print(y)
+n = int(input())
+h1 = n//3600
+h2 = n-h1*3600
+m1 = h2//60
+m2 = h2-m1*60
+s1 = m2
+print(str(h1)+":"+str(m1)+":"+str(s1))
