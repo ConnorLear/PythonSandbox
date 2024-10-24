7dbd242182958fa955ecc8147974675818501568
ConnorLear <connor.a.lear@gmail.com>
2024-10-24 20:29:51 +0000

    Update

--- kattus.py
+++ kattus.py
@@ -1,11 +1,4 @@
-list = []
 n = int(input())
-for i in range(n):
-    list1 = input()
-    p1 = list1[0:1].upper()
-    p2 = list1[1:].lower()
-    l1 = (p1+p2)
-    list.append(l1)
-    if i == n-1:
-        for j in range(len(list)):
-            print(list[j])
+x, z = n.split(" ")
+x+y/2 = z
+print(y)
