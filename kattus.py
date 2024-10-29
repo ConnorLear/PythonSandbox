2aa93b8e12ee4d8632f10aa02881cc4e0c3d4306
ConnorLear <connor.a.lear@gmail.com>
2024-10-29 21:42:27 +0000

    Update kattus.py

--- kattus.py
+++ kattus.py
@@ -2,9 +2,8 @@ n = "\__________________________________________________________________________
 print(len(n))
 p1 = n[1:]
 p2 = int(len(p1))/2
-if len(n) % 2 == 1:
-    for i in range(int(p2)+1):
-        if i == 0:
-            print(n[:-1]+"_/")
-        else:
-            print(" "*i+n[:1]+n[i*2:])
+for i in range(int(p2)+1):
+    if i == 0:
+        print(n[:-1]+"_/")
+    else:
+        print(" "*i+n[:1]+n[i*2:])
