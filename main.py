980f3101b9735f34ae93f8e29c8ffe9aee652238
ConnorLear <connor.a.lear@gmail.com>
2024-11-21 21:49:09 +0000

    Update

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+"""
+This is high enough game
+Connor Lear - Novermber 2024
+"""
+
 import random
 
-d1: int = random.randint(1,6)
-d2: int = random.randint(1,6)
-d3: int = random.randint(1,6)
+def main():
+    d1: int = random.randint(1,6)
+    d2: int = random.randint(1,6)
+    d3: int = random.randint(1,6)
 
-d4 = d1+d2+d3
+    d4 = d1+d2+d3
 
-print(d1)
-print(d2)
-print(d3)
-print(d4)
+    print(d1)
+    print(d2)
+    print(d3)
+    print(d4)
 
-if d4 > 13:
-    print("You win")
-else:
-    print("You lose")
+    if d4 > 13:
+        print("You win")
+    else:
+        print("You lose")
+
+
+if __name__ == "__main__":
+    main()
