5430900a97e201bce604cde6e0c98be4257e0279
ConnorLear <connor.a.lear@gmail.com>
2024-12-05 22:03:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 """
-This is high enough game
-Connor Lear - Novermber 2024
+Connor Lear - December 2024
 """
 
 import random
 
-def main():
-    d1: int = random.randint(1,6)
-    d2: int = random.randint(1,6)
-    d3: int = random.randint(1,6)
+def p(MAAAAAAPS):
+    print(MAAAAAAPS)
 
-    d4 = d1+d2+d3
+def roll_n_times(n: int) -> int:
+    print()
 
-    print(d1)
-    print(d2)
-    print(d3)
-    print(d4)
+prey_amount: int = 10
+preditor_amount: int = 1
+#You have to make prey and preditor names plurul
+prey_name: str = "Pigs"
+preditor_name: str = "People"
+most_childs = 5
+least_childs = 1
+max_preditor_kills = 2
+generations_one_turn = 1000
 
-    if d4 > 13:
-        print("You win")
-    else:
-        print("You lose")
+p(f"""There are {prey_amount} {prey_name}!
+There are {preditor_amount} {preditor_name}!
+This is the first {generations_one_turn} generations!""")
 
+p("")
 
-if __name__ == "__main__":
-    main()
+for i in range(generations_one_turn):
+	prey_births: int = random.randint(least_childs, most_childs)
+	prey_amount += prey_births
+	for i in range(preditor_amount):
+		preditor_kills: int = random.randint(1, max_preditor_kills)
+		if preditor_kills == 2:
+			preditor_amount += max_preditor_kills - 1
 
-def birthday(speed, birthday):
-    if birthday:
-        if speed <= 65:
-            return "no ticket"
-        elif speed <= 85:
-            return "small ticket"
-        else:
-            return "big ticket"
-    else:
-        if speed <= 60:
-            return "no ticket"
-        elif speed <= 80:
-            return "small ticket"
-        else:
-            return "big ticket"
-
-print(birthday())
+p(f"""There are now {prey_amount} {prey_name}!
+There are now {preditor_amount} {preditor_name}!
+This is the second {generations_one_turn} generations!""")
