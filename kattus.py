b746a22e5c39a0eb2f7142525cf81be58a24d650
ConnorLear <connor.a.lear@gmail.com>
2024-12-05 22:54:35 +0000

    Update

--- kattus.py
+++ kattus.py
@@ -1,15 +1,25 @@
-def p(GayMonkey):
-    print(GayMonkey)
 
-n = "\_______________________________________________/"
-p("""
-""")
-p1 = n[1:]
-p2 = int(len(p1))/2
-for i in range(int(p2)+1):
-    if i == 0:
-        p(n[:-1]+"_/")
-    else:
-        p(" "*i+n[:1]+n[i*2:])
-p("""
-""")
+
+data = input()
+
+rock = data.count("R")
+
+paper = data.count("P")
+
+scissors = data.count("S")
+
+winners = []
+
+if rock >= scissors and not(scissors >= rock):
+    winners.append("ROCK")
+
+if scissors >= paper and not(rock >= scissors):
+    winners.append("SCISSORS")
+
+if paper >= rock and not(scissors >= paper):
+    winners.append("PAPER")
+
+if len(winners) == 1:
+    print(winners[0])
+else:
+    print("NO WINNER")
--- main.py
+++ main.py
@@ -2,35 +2,41 @@
 Connor Lear - December 2024
 """
 
+
 import random
 
-def roll_n_times(n: int) -> int:
-    print()
 
-prey_amount: int = 100
+def roll_n_times(n: int) -> int:
+   print()
+
+
+prey_amount: int = 10
 preditor_amount: int = 1
 #You have to make prey and preditor names plurul
 prey_name: str = "Pigs"
 preditor_name: str = "People"
-most_childs = 5
+most_childs = 2
 least_childs = 1
 max_preditor_kills = 2
-generations_one_turn = 1000
-
+generations_one_turn = 8
 print(f"""There are {prey_amount} {prey_name}!
 There are {preditor_amount} {preditor_name}!
 This is the first {generations_one_turn} generations!""")
 
+
 print()
 
+
 for i in range(generations_one_turn):
-	prey_births: int = random.randint(least_childs, most_childs)
-	prey_amount += prey_births
+	for i in range(prey_amount):
+		prey_births: int = random.randint(least_childs, most_childs)
+		prey_amount += prey_births
 	for i in range(preditor_amount):
 		preditor_kills: int = random.randint(1, max_preditor_kills)
 		prey_amount -= preditor_kills
 		if preditor_kills == 2:
 			preditor_amount += max_preditor_kills - 1
+
 
 print(f"""There are now {prey_amount} {prey_name}!
 There are now {preditor_amount} {preditor_name}!
