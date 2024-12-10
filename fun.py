777a79adfb0d253f8547e9d9f1b038103e4d008b
ConnorLear <connor.a.lear@gmail.com>
2024-12-10 22:49:00 +0000

    Update fun

--- fun.py
+++ fun.py
@@ -5,3 +5,13 @@ for i in range(int(n)+1):
     m = str(f"2^{i}: {e}")
     print(m)
     e *= 2
+
+print()
+
+for j in range(int(n)):
+    t1 = int(n)-int(j)
+    t2 = int(j)*2
+    if int(j+1) < int(n):
+        print(f"{t1*" "}/{t2*" "}\ ")
+    else: 
+        print(f"{t1*" "}/{t2*"_"}\ ")
