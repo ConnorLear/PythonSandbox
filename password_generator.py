ee4b6353f694c6b03331daf98f68ee3d3d4176ca
ConnorLear <connor.a.lear@gmail.com>
2025-07-24 05:30:18 +0000

    More Words - pt: 1

--- password_generator.py
+++ password_generator.py
@@ -10,7 +10,17 @@ listw: list = ["pair", "abolish", "throat", "infection", "cunning",
 "archive", "press", "decade", "total", "name",
 "rotten", "truth", "legislature", "herb", "review",
 "alse", "opposition", "ladder", "chemistry", "reflection",
-"serve", "school", "gun", "straw", "cancer"]
+"serve", "school", "gun", "straw", "cancer",
+"sleep", "hurl", "trunk", "loss", "skin", 
+"lodge", "glow", "blade", "lot", "arrow", 
+"guest", "hard", "meat", "sign", "chart", 
+"watch", "plan", "diet", "suite", "tract", 
+"first", "gain", "sex", "wound", "opera", 
+"toss", "chest", "front", "quit", "trail", 
+"quota", "rank", "abbey", "crude", "bury", 
+"stain", "tooth", "old", "choke", "warn", 
+"urine", "spend", "cater", "wife", "index", 
+"exile", "land", "slam", "coup", "crown"]
 listn: list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 w = ""
